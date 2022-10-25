30ec09045df7ef6ca8cbeeaaa012c02f19ace1e9
windy <1519105541@qq.com>
2022-10-25 19:01:56 +0800

    test: 修改测试文件字段名

--- tests/unit/utils/validate.spec.js
+++ tests/unit/utils/validate.spec.js
@@ -1,10 +1,10 @@
-import { validUsername, isExternal } from '@/utils/validate.js'
+import { validLoginId, isExternal } from '@/utils/validate.js'
 
 describe('Utils:validate', () => {
-  it('validUsername', () => {
-    expect(validUsername('admin')).toBe(true)
-    expect(validUsername('editor')).toBe(true)
-    expect(validUsername('xxxx')).toBe(false)
+  it('validLoginId', () => {
+    expect(validLoginId('admin')).toBe(true)
+    expect(validLoginId('editor')).toBe(true)
+    expect(validLoginId('xxxx')).toBe(false)
   })
   it('isExternal', () => {
     expect(isExternal('https://github.com/PanJiaChen/vue-element-admin')).toBe(true)
