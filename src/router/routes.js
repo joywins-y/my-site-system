cf4936669a4e0653a16dec9320eff75372191012
windy <1519105541@qq.com>
2022-10-26 17:13:08 +0800

    feat: 路由调整

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,3 @@
-/* Layout */
 import Layout from '@/layout'
 
 export default [
@@ -23,141 +22,19 @@ export default [
         path: 'dashboard',
         name: 'Dashboard',
         component: () => import('@/views/dashboard/index'),
-        meta: { title: 'Dashboard', icon: 'dashboard' }
-      }
-    ]
-  },
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
-
-  {
-    path: '/form',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
-  },
-
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () =>
-                  import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () =>
-                  import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
-      }
-    ]
-  },
-
-  {
-    path: 'external-link',
-    component: Layout,
-    children: [
-      {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
-  },
-
-  // 正文------------------------------
-
-  {
-    path: '/',
-    component: Layout,
-    children: [
-      {
-        path: 'control',
-        name: 'Control',
-        component: () => import('@/views/control/index'),
         meta: { title: '控制台', icon: 'dashboard', auth: true }
       }
     ]
   },
 
   {
-    path: '/',
+    path: '/banner',
     component: Layout,
     children: [
       {
-        path: 'home',
-        name: 'Home',
-        component: () => import('@/views/home/index'),
+        path: 'index',
+        name: 'Banner',
+        component: () => import('@/views/banner/index'),
         meta: { title: '首页标语', icon: 'el-icon-s-home', auth: true }
       }
     ]
@@ -168,99 +45,123 @@ export default [
     component: Layout,
     redirect: '/blog',
     name: 'Blog',
-    meta: { title: '文章管理', icon: 'el-icon-notebook-2' },
+    meta: { title: '文章管理', icon: 'el-icon-document' },
     children: [
       {
         path: '/blogList',
         name: 'BlogList',
         component: () => import('@/views/blogList/index'),
-        meta: { title: '文章列表', icon: 'el-icon-tickets', auth: true }
+        meta: { title: '文章列表', icon: 'el-icon-s-order', auth: true }
       },
       {
         path: '/blogType',
         name: 'BlogType',
         component: () => import('@/views/blogType/index'),
-        meta: { title: '文章类型', icon: 'el-icon-c-scale-to-original', auth: true }
+        meta: { title: '文章分类', icon: 'el-icon-menu', auth: true }
       },
       {
         path: '/addBlog',
         name: 'AddBlog',
         component: () => import('@/views/addBlog/index'),
-        meta: { title: '添加文章', icon: 'el-icon-edit-outline', auth: true }
-      }
-    ]
-  },
-
-  {
-    path: '/project',
-    component: Layout,
-    redirect: '/project',
-    name: 'Project',
-    meta: { title: '项目管理', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: '/projectList',
-        name: 'ProjectList',
-        component: () => import('@/views/blogList/index'),
-        meta: { title: '项目列表', icon: 'el-icon-s-operation', auth: true }
+        meta: { title: '添加文章', icon: 'el-icon-circle-plus', auth: true }
       },
       {
-        path: '/projectType',
-        name: 'ProjectType',
-        component: () => import('@/views/blogList/index'),
-        meta: { title: '项目分类', icon: 'el-icon-folder-opened', auth: true }
+        path: '/editBlog/:id',
+        name: 'editBlog',
+        hidden: true,
+        component: () => import('@/views/editBlog/index'),
+        meta: { title: '编辑文章', icon: 'el-icon-circle-plus', auth: true }
       }
     ]
   },
 
   {
-    path: '/',
+    path: '/projects',
+    component: Layout,
+    redirect: '/projects',
+    name: 'Projects',
+    meta: { title: '项目管理', icon: 'el-icon-suitcase' },
+    children: [
+      {
+        path: '/projectsList',
+        name: 'ProjectsList',
+        component: () => import('@/views/projects/index'),
+        meta: { title: '项目列表', icon: 'el-icon-notebook-1', auth: true }
+      },
+      {
+        path: '/addProject',
+        name: 'AddProject',
+        component: () => import('@/views/addProject/index'),
+        meta: { title: '添加项目', icon: 'el-icon-link', auth: true }
+      }
+    ]
+  },
+  {
+    path: '/comment',
     component: Layout,
     children: [
       {
-        path: 'comment',
+        path: 'index',
         name: 'Comment',
         component: () => import('@/views/comment/index'),
-        meta: { title: '评论管理', icon: 'el-icon-s-comment', auth: true }
+        meta: {
+          title: '评论管理',
+          icon: 'el-icon-chat-line-round',
+          auth: true
+        }
       }
     ]
   },
-
   {
-    path: '/',
+    path: '/message',
     component: Layout,
     children: [
       {
-        path: 'message',
+        path: 'index',
         name: 'Message',
         component: () => import('@/views/message/index'),
-        meta: { title: '留言板', icon: 'el-icon-message-solid', auth: true }
+        meta: { title: '留言板', icon: 'el-icon-copy-document', auth: true }
       }
     ]
   },
 
   {
-    path: '/',
+    path: '/about',
     component: Layout,
     children: [
       {
-        path: 'about',
+        path: 'index',
         name: 'About',
         component: () => import('@/views/about/index'),
-        meta: { title: '关于我', icon: 'el-icon-user-solid', auth: true }
+        meta: { title: '关于我', icon: 'el-icon-s-custom', auth: true }
       }
     ]
   },
 
   {
-    path: '/',
+    path: '/setting',
     component: Layout,
     children: [
       {
-        path: 'setting',
+        path: 'index',
         name: 'Setting',
         component: () => import('@/views/setting/index'),
         meta: { title: '设置', icon: 'el-icon-s-tools', auth: true }
+      }
+    ]
+  },
+
+  {
+    path: '/personalSetting',
+    component: Layout,
+    hidden: true,
+    meta: { children: ['/personalSetting'] },
+    children: [
+      {
+        path: '/personalSetting',
+        name: 'PersonalSetting',
+        component: () => import('@/views/personalSetting/index'),
+        meta: { title: '个人中心', icon: 'el-icon-document-copy', auth: true }
       }
     ]
   },
