ae08b32bd946139392113aad30b1a84994931161
windy <1519105541@qq.com>
2022-10-28 17:52:12 +0800

    feat: 新增banner列表和编辑功能

---
+++ src/components/UploadFile/index.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="upload-container">
+        <div class="block">{{ title }}</div>
+        <el-upload class="avatar-uploader" action="/api/upload" :show-file-list="false" :on-success="handleSuccess"
+            :before-upload="beforeUpload" :headers="headers">
+            <img v-if="value" :src="imageUrl" alt="img" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+    </div>
+</template>
+
+<script>
+import { getToken } from '@/utils/auth';
+import { server_URL } from '@/utils/url';
+
+export default {
+    props: {
+        title: { type: String, default: "" },
+        value: { type: String, default: "" },
+    },
+    computed: {
+        imageUrl() {
+            if (this.value) {
+                const prefix = this.value.indexOf('http') === -1 ? server_URL : '';
+                return prefix + this.value;
+            }
+        },
+        headers() {
+            return {
+                Authorization: "Bearer " + getToken()
+            }
+        }
+    },
+    methods: {
+        handleSuccess(res, file) {
+            if (!res.code && res.data) {
+                this.$emit('input', res.data)
+            }
+        },
+        beforeUpload(file) {
+            const isJPG = file.type === 'image/jpeg';
+            const isLt2M = file.size / 1024 / 1024 < 2;
+
+            if (!isJPG) {
+                this.$message.error('上传头像图片只能是 JPG 格式！');
+            }
+            if (!isLt2M) {
+                this.$message.error('上传头像图片大小不能超过 2MB!');
+            }
+            return isJPG && isLt2M;
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.upload-container {
+    .block {
+        margin: 16px 0;
+        font-weight: 100;
+    }
+
+    .avatar-uploader .el-upload {
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        position: relative;
+        overflow: hidden;
+        cursor: pointer;
+
+        &:hover {
+            border-color: #409EFF;
+        }
+    }
+
+    .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 178px;
+        line-height: 178px;
+        text-align: center;
+    }
+
+    .avatar {
+        width: 178px;
+        height: 100%;
+        display: block;
+    }
+}
+</style>
--- src/views/banner/index.vue
+++ src/views/banner/index.vue
@@ -1,15 +1,128 @@
 <template>
-  <div class="">
-    Banner
+  <div class="banner-container">
+    <!-- 数据表格 -->
+    <el-table :data="data" style="width: 100%;">
+      <el-table-column type="index" label="序号" width="100" align="center"></el-table-column>
+      <el-table-column prop="title" label="标题"></el-table-column>
+      <el-table-column prop="description" label="描述"></el-table-column>
+      <el-table-column prop="midImg" label="小图">
+        <template slot-scope="scope">
+          <img :src="scope.row.bigImg" alt="" style="width: 100px; height: 100%;">
+        </template>
+      </el-table-column>
+      <el-table-column prop="bigImg" label="大图">
+        <template slot-scope="scope">
+          <el-image :src="scope.row.bigImg" style="width: 100px; height: 100%" fit="fill"></el-image>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-tooltip class="item" effect="dark" content="编辑" placement="top" :hide-after="2000">
+            <el-button type="primary" size="mini" icon="el-icon-edit" circle
+              @click="handleEdit(scope.$index, scope.row)"></el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 弹出编辑框 -->
+
+    <el-dialog title="编辑" :visible.sync="visible" :close-on-click-modal="false" :destroy-on-close="true">
+      <el-form :model="form" label-position="top">
+        <el-form-item label="标题" :label-width="labelWidth">
+          <el-input v-model="form.title"></el-input>
+        </el-form-item>
+        <el-form-item label="描述" :label-width="labelWidth">
+          <el-input type="textarea" v-model="form.description" rows="4"></el-input>
+        </el-form-item>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="小图">
+              <UploadFile v-model="form.midImg" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="大图">
+              <UploadFile v-model="form.bigImg" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+
+      <div class="dialog-footer" slot="footer">
+        <el-button @click="visible = false">取消</el-button>
+        <el-button @click="handleConfirm" type="primary">确定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
-export default {
+import { getBanner, setBanner } from '@/api/banner';
+// import { server_URL } from '@/utils/url';
+import UploadFile from '@/components/UploadFile';
 
+export default {
+  props: {},
+  components: { UploadFile },
+  data() {
+    return {
+      data: [],
+      visible: false,
+      form: {
+        id: '',
+        midImg: '',
+        bigImg: '',
+        title: '',
+        description: ''
+      },
+      labelWidth: '120px'
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  // computed: {
+  //   imageUrl(){
+  //     if(this.value){
+
+  //     }
+  //   } 
+  // },
+  methods: {
+    fetchData() {
+      getBanner().then(res => {
+        console.log(res);
+        this.data = res.data;
+        // for (const item of this.data) {
+        //   item.minImg = server_URL + item.midImg;
+        //   item.maxImg = server_URL + item.bigImg;
+        // }
+      })
+    },
+    handleEdit(index, record) {
+      this.form = { ...record };
+      this.visible = true;
+    },
+    handleConfirm() {
+      let arr = [...this.data];
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i].id == this.form.id) {
+          arr[i] = this.form;
+        }
+      }
+
+      setBanner(arr).then(res => {
+        this.visible = false;
+        this.$message({ type: 'success', message: "修改成功！" })
+        this.fetchData();
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 
 </style>
